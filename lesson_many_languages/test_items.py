bddc83d6ec63965bc36edde22f609573e2c3faed
Julia Kamysheva <jkamysheva@gmail.com>
2025-02-13 20:04:02 +0300

    название теста более корректное

--- lesson_many_languages/test_items.py
+++ lesson_many_languages/test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 import time
 
-def test_different_languages(browser):
+def test_btn_add_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(10)
     assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket'), 'Кнопки добавления в корзину нет'
